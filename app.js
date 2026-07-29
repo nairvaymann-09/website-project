1d9fedfc694ef6c75b786c19814e870e2744cc76
nairvaymann-09 <nairvaymann@gmail.com>
2026-07-30 02:59:03 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function showAboutMe() {
+    var name = document.querySelector("#About");
+    name.textContent = "Naira ayman 16y my dream is to become a software engineer";
+    var box = document.querySelector(".style");
+    box.style.color = "whit";
+    box.style.backgroundColor = "black";
+    box.style.padding = "10px";
+    box.style.fontSize = "50px";
+}
